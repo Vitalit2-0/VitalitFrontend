0f0f02ba03a00c8f485dd2040c4c50bfa8caaac7
Tomas <tparra@unal.edu.co>
2024-04-10 23:05:40 -0500

    ClassName opcional - arreglo componente Dot

--- src/components/stylers/Dot.tsx
+++ src/components/stylers/Dot.tsx
@@ -1,4 +1,4 @@
-function Dot({ width, className, color, top, right, bottom, left } : { width: string, className: string, color: string, top?: string, right?: string, bottom?: string, left?: string}) {
+function Dot({ width, className, color, top, right, bottom, left } : { width: string, className?: string, color: string, top?: string, right?: string, bottom?: string, left?: string}) {
     return (
         <div className={`absolute circular ${className}`} style={{width: width, height: width, background: color, top: (top) ? top : "auto", right: (right) ? right : "auto", bottom: (bottom) ? bottom : "auto", left: (left) ? left : "auto" }}></div>
     )
