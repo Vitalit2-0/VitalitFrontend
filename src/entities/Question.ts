fd6099a2ab7cb272097e71737f81cb0a2cda6c48
Tomas <tparra@unal.edu.co>
2024-04-21 11:05:09 -0500

    Implementación de librerías para información general de usuario
    Instalacion de MUI-X para uso de Datepickers
    Instalacion de toastify para notificaciones

--- src/entities/Question.ts
+++ src/entities/Question.ts
@@ -2,7 +2,6 @@ type Question = {
     id: string;
     question: string;
     questionType: string;
-    category: string;
     options?: string[];
     visible: boolean;
     selectedOptions?: number[];
