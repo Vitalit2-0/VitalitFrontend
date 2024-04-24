097d302051de952a24b0d86fe17f818627c067e8
Julian <julianavg2002@gmail.com>
2024-04-23 19:36:02 -0500

    Notificacion-cambio-fecha-hora

--- src/components/ModalNotification.tsx
+++ src/components/ModalNotification.tsx
@@ -1,18 +1,47 @@
-import { Box, Modal, Typography } from '@mui/material'
+import { Box, Button, Modal, Typography } from '@mui/material'
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import dayjs from 'dayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimeClock } from '@mui/x-date-pickers/TimeClock';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 function ModalNotification({ openDate, setOpenDate, style, section } : { openDate: boolean, setOpenDate: any, style: any, section: string}) {
 
+    const [selectedDays, setSelectedDays] = useState<string[]>([]);
+    const [selectedTime, setSelectedTime] = useState(null);
 
     const getCheckboxLabel = () => {
         return `No quiero recibir notificaciones de la sección ${section}`;
+    };
+
+    const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.checked) {
+            setSelectedDays([...selectedDays, event.target.value]);
+        } else {
+            setSelectedDays(selectedDays.filter((day: string) => day !== event.target.value));
+        }
+    };
+
+    const handleTimeChange = (time: any) => {
+        setSelectedTime(time);
+    };
+
+        const handleSaveChanges = () => {
+        let message = 'Cambios guardados correctamente!';
+
+        if (selectedDays.length > 0) {
+            message += ` Recibiras notificaciones los días: ${selectedDays.join(', ')}.`;
+        }
+
+        if (selectedTime) {
+            message += ` A la hora: ${selectedTime}.`;
+        }
+
+        toast.success(message);
     };
 
     return (
@@ -33,43 +62,43 @@ function ModalNotification({ openDate, setOpenDate, style, section } : { openDat
                     <FormGroup aria-label="position" row>
                         <FormControlLabel
                             value="lunes"
-                            control={<Checkbox />}
+                            control={<Checkbox onChange={handleCheckboxChange}/>}
                             label="Lunes"
                             labelPlacement="top"
                         />
                         <FormControlLabel
                             value="martes"
-                            control={<Checkbox />}
+                            control={<Checkbox onChange={handleCheckboxChange}/>}
                             label="Martes"
                             labelPlacement="top"
                         />
                         <FormControlLabel
                             value="miercoles"
-                            control={<Checkbox />}
+                            control={<Checkbox onChange={handleCheckboxChange}/>}
                             label="Miércoles"
                             labelPlacement="top"
                         />
                         <FormControlLabel
                             value="jueves"
-                            control={<Checkbox />}
+                            control={<Checkbox onChange={handleCheckboxChange}/>}
                             label="Jueves"
                             labelPlacement="top"
                         />
                         <FormControlLabel
                             value="viernes"
-                            control={<Checkbox />}
+                            control={<Checkbox onChange={handleCheckboxChange}/>}
                             label="Viernes"
                             labelPlacement="top"
                         />
                         <FormControlLabel
                             value="sabado"
-                            control={<Checkbox />}
+                            control={<Checkbox onChange={handleCheckboxChange}/>}
                             label="Sábado"
                             labelPlacement="top"
                         />
                         <FormControlLabel
                             value="domingo"
-                            control={<Checkbox />}
+                            control={<Checkbox onChange={handleCheckboxChange}/>}
                             label="Domingo"
                             labelPlacement="top"
                         />
@@ -79,7 +108,7 @@ function ModalNotification({ openDate, setOpenDate, style, section } : { openDat
                 <p>Selecciona la hora</p>
 
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <TimeClock defaultValue={dayjs('2022-04-17T15:30')} ampm={false} />
+                    <TimeClock ampm={false} onChange={handleTimeChange}/>
                 </LocalizationProvider>
 
                 <FormControl component="fieldset">
@@ -90,6 +119,8 @@ function ModalNotification({ openDate, setOpenDate, style, section } : { openDat
                             labelPlacement="top"
                         />
                 </FormControl>
+
+                <Button onClick={handleSaveChanges}>Guardar cambios</Button>
             </Box>
         </Modal>
     )
