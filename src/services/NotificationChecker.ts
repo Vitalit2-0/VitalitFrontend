37f29e4d6ab3c54ae72d0962530fc24e5adb2c5d
JulianAVG64 <julvargasgu@unal.edu.co>
2024-04-29 00:13:57 -0500

    Correcion ciclos notificaciones

--- src/services/NotificationChecker.ts
+++ src/services/NotificationChecker.ts
@@ -2,22 +2,33 @@ import { toast } from 'react-toastify';
 import { getNotificationContent } from './NotificationDataProvider';
 
 export class NotificationChecker {
+    private shownNotifications: { [key: string]: boolean } = {};
     checkNotification(userParams: { day: string[], time: string, section: string }) {
-        // Run every 30 minutes
+        // Run every 10 seconds
         setInterval(async () => {
             const now = new Date();
             const dayMapping = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
             const currentDay = dayMapping[now.getDay()];
-            const currentTime = now.getHours().toString() + ":" + now.getMinutes().toString();
+            const currentTime = now.getHours().toString().padStart(2, '0') + ":" + now.getMinutes().toString().padStart(2, '0');
 
             if (userParams.day.includes(currentDay) && userParams.time === currentTime) {
-                const notifications = await getNotificationContent();
-                const notification = notifications.find(n => n.section === userParams.section);
-                if (notification) {
-                    // Display notification using react-toastify
-                    toast(`${notification.title}: ${notification.message}`);
+                // Check if the notification has already been shown today
+                const notificationKey = `${currentDay}-${userParams.section}`;
+                if (!this.shownNotifications[notificationKey]) {
+                    const notifications = await getNotificationContent();
+                    const notification = notifications.find(n => n.section === userParams.section);
+                    if (notification) {
+                        // Display notification using react-toastify
+                        toast(`${notification.title}: ${notification.message}`);
+                        //Mark the notification as shown
+                        this.shownNotifications[notificationKey] = true;
+                    }
                 }
+                
+            } else if (currentTime === '00:00') {
+                // Reset shown notifications at midnight
+                this.shownNotifications = {};
             }
-        }, 10000); // 30 minutes in milliseconds
+        }, 10000); // 10 seconds in milliseconds
     }
 }
